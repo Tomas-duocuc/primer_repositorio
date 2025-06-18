70e1fd865062f16b5b92024678a6b28c28c07733
Jaime132132 <to.rodriguezl@duocuc.cl>
2025-06-18 19:27:29 -0400

    parte 1

--- Formulario_4.py
+++ Formulario_4.py
@@ -11,8 +11,46 @@ turistas = {"001": ["John Doe", "Estados Unidos", "12-01-2024"],
             "010": ["Martin Fernandez", "Argentina", "13-02-2023"],
             "011": ["Sofia Gomez", "Argentina", "07-04-2024"],
     }
+def turistas_del_pais(pais):
+        for i in turistas:
+            paises=(turistas[i][1])
+            if paises==pais:
+                print(turistas[i][0])
 
-def turistas_del_pais():
-    for i in turistas:
-        pais=(print("paises: ", turistas[i][1]))
-turistas_del_pais()
+def turistas_por_mes():
+    
+opcion=0
+while opcion!=4:
+    print("*** MENU PRINCIPAL ***")
+    print("1.- Turistas por país.")
+    print("2.- Turista por mes.")
+    print("3.- Eliminar turista.")
+    print("4.- Salir")
+    opcion=int(input("Ingrese la opcion que desea: "))
+    match opcion:
+        case 1:
+            print("Seleccione el pais que desea: ")
+            print("1.- Estados Unidos")
+            print("2.- Argentina")
+            print("3.- Mexico")
+            print("4.- Brasil")
+            print("5.- Salir")
+            pais=int(input(""))
+            match pais:
+                case 1:
+                    pais="Estados Unidos"
+                case 2:
+                    pais="Argentina"
+                case 3:
+                    pais="Mexico"
+                case 4:
+                    pais="Brasil"
+                case 5:
+                    break
+                case _:
+                    print("Eliga una de las opciones mencionadas")
+            turistas_del_pais(pais)
+        case 2:
+            
+
+
